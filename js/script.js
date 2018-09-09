a5dfe0d285ef0310777b3f34701ffcf14e420676
Nick L. Ruiz <Nicklom23@gmail.com>
2018-09-09 15:30:53 -0500

    verison de pagina 1 no responsive

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// window.onload = fuction(){
+// 	var contenedor = document.getElementById('textoPreloader');
+// 	contenedor.style.visibility = 'hidden';
+// 	contenedor.style.opacity = '0';
+// }
+
+$('.carousel').carousel({
+	interval: 3000,
+	pause: "hover",
+})
